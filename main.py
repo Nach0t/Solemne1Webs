ebd02784c61d37fe7998e5f606bb1eb1069dc314
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2025-04-02 12:55:30 -0300

    update 1

---
+++ main.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get(
+    "/time",
+    summary="Obtener la fecha y hora actual",
+    description="Devuelve la fecha y hora actual en formato JSON.",
+)
+async def get_time():
+    """
+    Retorna la fecha y hora actual en formato Año-Mes-Día Hora:Minuto:Segundo.
+    """
+    now = datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return {"time": formatted_time}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
