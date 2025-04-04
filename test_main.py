9c2b9d915d51d6ef261d7632e5e5e1eaef6892b3
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2025-04-04 13:04:38 -0300

    a

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_time():
+    response = client.get("/time")
+    assert response.status_code == 200
+    assert "time" in response.json()
